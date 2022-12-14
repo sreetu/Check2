766aaae1bd037281d7e7e619e2225c6753e23252
reetu.singh <reetu.singh@PS-LP-NOI-12345>
2022-12-14 12:19:54 +0530

    Add project files.

---
+++ Check1/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Check1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Check1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+
+}
